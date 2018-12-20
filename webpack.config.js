d05a3f3253012460768b1a2a2c7d1777309d00da
Russ Biggs <russbiggs@gmail.com>
2018-12-20 15:48:38 -0700

    project boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    port: 9000
+  },
+  plugins: [new MinifyPlugin()],
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
